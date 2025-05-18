3b6f6b4514685f430d8125c682850538f2e66f66
iPromKnight <promknight@proton.me>
2025-05-18 02:38:53 +0100

    feat: ensure we install latest rclone - rclone manager wont change

--- apps/rclone-manager/ci/latest.sh
+++ apps/rclone-manager/ci/latest.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-version="main"
+version=$(curl -Lsf https://api.github.com/repos/rclone/rclone/releases/latest | jq --raw-output '. | .tag_name')
+version="${version#*v}"
 printf "%s" "${version}"
