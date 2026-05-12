c7f93fe15361bf903490c33246cd31a5df6645e1
iPromKnight <156901906+iPromKnight@users.noreply.github.com>
2026-05-12 20:58:26 +0100

    chore: remove cleanuparr, and overseerr, and bump iumagemaid to bookworm

--- apps/cleanuparr/ci/latest.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-AUTH="Authorization: token ${GH_PAT:-${TOKEN:-}}"
-version=$(curl -sLf -H "$AUTH" https://api.github.com/repos/Cleanuparr/Cleanuparr/releases/latest | jq --raw-output '. | .tag_name')
-version="${version#*v}"
-printf "%s" "${version}"
