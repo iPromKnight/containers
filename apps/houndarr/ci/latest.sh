ff61fd290dc26c5f94a4c32cdf20658ae673cfb0
iPromKnight <156901906+iPromKnight@users.noreply.github.com>
2026-05-13 20:42:34 +0100

    feat: add houndarr

---
+++ apps/houndarr/ci/latest.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+AUTH="Authorization: token ${GH_PAT:-${TOKEN:-}}"
+version=$(curl -sLf -H "$AUTH" https://api.github.com/repos/av1155/houndarr/releases/latest | jq --raw-output '. | .tag_name')
+printf "%s" "${version}"
